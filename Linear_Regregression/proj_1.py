6c12d1f8a9d7ad637b6dc36500aa371f0e10bf78
Dinesh Dawonauth <dineshdscience@gmail.com>
2022-12-24 04:52:39 -0500

    Added model saving feature using pickle

--- Linear_Regregression/proj_1.py
+++ Linear_Regregression/proj_1.py
@@ -1,8 +1,12 @@
-import pandas as pd
+import pickle
+import sys
+from time import sleep
 import numpy as np
+import pandas as pd
 import sklearn
 from sklearn import linear_model
 
+ACCURACY = 0
 
 # Reading data
 data = pd.read_csv("student-mat.csv", sep=";")
@@ -13,15 +17,30 @@ predict = "G3"
 attributes = np.array(data.drop(columns=[predict]))
 label = np.array(data[predict])
 
-attributes_train, attributes_test, label_train, label_test = sklearn.model_selection.train_test_split(attributes, label, test_size=0.1)
+for i in range(100):
+    attributes_train, attributes_test, label_train, label_test = sklearn.model_selection.train_test_split(attributes, label, test_size=0.1)
 
-linear = linear_model.LinearRegression()
-linear.fit(attributes_train, label_train)
-accuracy = linear.score(attributes_test, label_test)
+    linear = linear_model.LinearRegression()
+    linear.fit(attributes_train, label_train)
+    accuracy = linear.score(attributes_test, label_test)
 
-print("Accuracy: %.2f%%" % (100 * accuracy))
+    sys.stdout.write("\rAccuracy: %.2f%%" % (100 * accuracy))
+    sys.stdout.flush()
+    sleep(0.09)
+
+    if accuracy > ACCURACY:
+        ACCURACY = accuracy
+        with open("student_grade_predictions", "wb") as f:
+            pickle.dump(linear, f)
+
+
+pickle_student_model = open("student_grade_predictions", "rb")
+linear = pickle.load(pickle_student_model)
+
+print("\nFinal Accuracy: %.2f%%" % (100 * ACCURACY))
 
 predictions = linear.predict(attributes_test)
 
+# Printing predictions alongside the actual data
 for i in range(len(predictions)):
     print(f"Prediction: {predictions[i]}, | Data: {attributes_test[i]} |  Actual Value:  {label_test[i]}")
